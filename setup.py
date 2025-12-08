75412253e1013c0b470e025ef02763d759a6f805
Mahika <mahikasharma0007@gmail.com>
2025-12-08 23:15:53 +0530

    code added in .\requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 from setuptools import setup, find_packages
 
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path):
+    """
+    Reads a requirements.txt file and returns a list of dependencies.
+    Ignores empty lines and comments.
+    """
+    requirements = []
+    with open(file_path) as file:
+        requirements=file.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
 setup(
     name="predictive_maintenance_ai4i",
     version="1.0.0",
@@ -9,20 +25,6 @@ setup(
     long_description_content_type="text/markdown",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        "pandas",
-        "numpy",
-        "scikit-learn",
-        "matplotlib",
-        "seaborn",
-        "xgboost",
-        "imbalanced-learn",
-        "joblib"
-    ],
-    python_requires=">=3.8",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    install_requires=get_requirements('requirements.txt')
+    
 )
